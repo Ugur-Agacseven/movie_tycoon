c34f90e79fc7095db46f78dc652ae1e836405d75
CapFox <agacseven.ugur@gmail.com>
2024-09-14 15:30:57 +0200

    Init

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\agacs\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\agacs\\StudioProjects\\movie_tycoon" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\agacs\\flutter"
+  "PROJECT_DIR=C:\\Users\\agacs\\StudioProjects\\movie_tycoon"
+  "FLUTTER_ROOT=C:\\Users\\agacs\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\agacs\\StudioProjects\\movie_tycoon\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\agacs\\StudioProjects\\movie_tycoon"
+  "FLUTTER_TARGET=C:\\Users\\agacs\\StudioProjects\\movie_tycoon\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\agacs\\StudioProjects\\movie_tycoon\\.dart_tool\\package_config.json"
+)
